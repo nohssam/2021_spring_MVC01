0943cc89e01a142ce6f8c1d3f76b226d461fb04d
nohssam <nohssam73@gmail.com>
2021-07-08 17:51:30 +0900

    first

---
+++ src/main/java/com/ict/controller/SayController.java
@@ -0,0 +1,15 @@
+package com.ict.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+//어노테이션이 아니면 무조건 Controller 를 상속받아야 한다.
+public class SayController implements Controller{
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("result3");
+	}
+}
